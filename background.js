54280cc6dadf25ddedc96a4ab7fbb374c0cf26e1
Nicholas Jitkoff <nicholas@jitkoff.com>
2021-06-20 08:31:17 -0700

    Remove queryparams based group restoring

--- background.js
+++ background.js
@@ -1,8 +1,6 @@
 chrome.runtime.onInstalled.addListener(function() {
   console.log("Polychrome Installed");
-
 });
-
 
 chrome.commands.onCommand.addListener((command) => {
   if (command == "search") {
@@ -13,6 +11,10 @@ chrome.commands.onCommand.addListener((command) => {
 });
 
 
+//
+// Window Management
+//
+
 const SEARCH_WIDTH = 320;
 async function toggleSearch() {
   let url = chrome.runtime.getURL("search.html");
@@ -20,9 +22,7 @@ async function toggleSearch() {
   let win = await chrome.windows.getLastFocused({populate:false, windowTypes:['normal']})
   if (tabs && tabs.length) {
       let sidebarId = tabs[0].windowId;
-//      let sidebar = await chrome.windows.get(sidebarId)
       chrome.windows.update(sidebarId, {focused:true})
-
   } else {
     await chrome.windows.create({
       url: url,
@@ -33,7 +33,6 @@ async function toggleSearch() {
       top:win.top + 7,
     });
   }
-
 }
 
 
@@ -74,8 +73,6 @@ async function toggleSidebar() {
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   let handlers = {
-    restoreGroup: restoreGroup,
-    reload: reload,
     focusSidebar: focusSidebar,
     'removeDuplicates': removeDuplicates
   }
@@ -89,10 +86,6 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     sendResponse(undefined)
   }
 });
-
-function reload() {
-  //chrome.runtime.reload()
-}
 
 function removeDuplicates() {
   chrome.windows.getAll({populate:true, windowTypes:['normal']})
@@ -111,65 +104,3 @@ function removeDuplicates() {
     })
   })
 }
-
-function restoreGroup(args, sender, sendResponse) {
-  let url = new URL(sender.url);
-  let id = url.hash.substr(1);
-  let params = new URLSearchParams(url.search);
-  if (params) {
-    restoreGroupWithBookmark(params)
-  }
-}
-
-function restoreGroupWithBookmark(params) { 
-  let id = params.get('id');
-  let color = params.get('color');
-  let urls = JSON.parse(params.get('urls'));
-  let title = params.get('title');
-
-  return chrome.tabGroups.query({title: title, color:color})
-  .then (groups => {
-    // let group = groups[0];
-    // if (group) {
-    //   console.log("group", group, {groupId:group.id, windowId:group.windowId})
-    //   let m = chrome.tabs.query({windowId:group.windowId})
-    //   .then(tabs => {
-    //     console.log("found", tabs)
-    //   })
-    //   return;
-    // }
-  
-    // let title = folder.title;
-    // return chrome.bookmarks.getChildren(id)
-    // .then ((children) => {
-    //   console.log(`Restoring Bookmark Group [${folder.title}]` )
-
-    //   let promises = [];
-    //   children.forEach(bookmark => {
-    //     if (bookmark.url.startsWith("chrome-extension://")) return;
-    //     promises.push(chrome.tabs.create({url: bookmark.url, selected:false, active:false}))
-    //   })
-    //   return Promise.all(promises);
-
-    // })
-
-    console.log(`Restoring Bookmark Group [${title}]` )
-
-    let promises = [];
-    urls.forEach((url,i) => {
-      let promise = chrome.tabs.create({url: url, selected:false, active:false})
-      //if (i > 0) promise = promise.then(tab => chrome.tabs.discard(tab.id))
-      promises.push(promise)
-    })
-    Promise.all(promises)
-    .then (tabs => {
-      return chrome.tabs.group({tabIds:tabs.map(t => t.id), createProperties:{}})
-      .then((gid) => {
-        chrome.tabs.update(tabs[0].id, { 'active': true });
-        chrome.tabGroups.update(gid, {title:title, color:color})
-      })
-      
-    }); 
-  })
-}
-
--- open.js
+++
@@ -1,8 +0,0 @@
-chrome.runtime.sendMessage({action:'restoreGroup', url:window.location}, (response) => {
-  chrome.tabs.getCurrent().then(tab => {
-    chrome.tabs.goBack(tab.id)
-    .catch(() => {
-      chrome.tabs.remove(tab.id);
-    })
-  })
-});
